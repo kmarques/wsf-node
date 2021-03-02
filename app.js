4f69d729f03c83e2592a1c51730cfbaf8a231d7a
Karl MARQUES <kmarques@vetixy.com>
2021-03-02 09:33:23 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const VehiculeRouter = require("./routes/vehicule");
+const cors = require("cors");
 
 app.use(bodyparser.json());
+//app.use(cors());
 
 app.use(VehiculeRouter);
 
-app.listen(3000);
+app.listen(3009);
